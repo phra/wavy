5ab97257de692703b30c9677e8417bc4c8407c05
phra <greensoncio@gmail.com>
2017-09-07 15:11:13 +0200

    fix path slice when node_modules is not present

--- postinstall.js
+++ postinstall.js
@@ -2,7 +2,13 @@ var fs = require('fs');
 var path = require('path');
 
 var dirname = __dirname.replace(/\\/g, '/');
-var root = path.resolve(dirname.slice(0, dirname.lastIndexOf('/node_modules/')));
+var index = dirname.lastIndexOf('/node_modules/');
+var root;
+if (index >= 0) {
+  root = path.resolve(dirname.slice(0, index >= 0 ? index : 0));
+} else {
+  root = dirname
+}
 var link = root + '/node_modules/~';
 try {
   var existingReal = path.resolve(fs.realpathSync(link));
@@ -11,5 +17,6 @@ try {
   process.exit(0);
 }
 if (existingReal && existingReal !== root) {
-  throw new Error(link + ' is already being used')
+  throw new Error(link + ' is already being used');
 }
+

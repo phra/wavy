2da4cfe67ac112a39f077316d197b32e33785dd7
phra <greensoncio@gmail.com>
2017-09-07 22:55:14 +0200

    fix: create node_modules if not existing in postinstall

--- postinstall.js
+++ postinstall.js
@@ -5,7 +5,7 @@ var dirname = __dirname.replace(/\\/g, '/');
 var index = dirname.lastIndexOf('/node_modules/');
 var root;
 if (index >= 0) {
-  root = path.resolve(dirname.slice(0, index >= 0 ? index : 0));
+  root = path.resolve(dirname.slice(0, index));
 } else {
   root = dirname
 }
@@ -13,10 +13,10 @@ var link = root + '/node_modules/~';
 try {
   var existingReal = path.resolve(fs.realpathSync(link));
 } catch (e) {
+  fs.mkdirSync(root + '/node_modules')
   fs.symlinkSync(root, link, 'junction');
   process.exit(0);
 }
 if (existingReal && existingReal !== root) {
   throw new Error(link + ' is already being used');
 }
-
